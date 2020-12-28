d294ae414cc76fe923a00d5647d515a733e7d38d
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2020-12-28 05:55:15 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 
-import fs from 'fs'
+// TODO
+// + Inline favicon.svg and manifest.webmanifest as data URLs
+// + Optimize gtm.js
+
+import { readFileSync } from 'fs'
 import { body, html, link, meta, noscript, script, style, title, div } from './html'
-
-const readFile = file => fs.readFileSync(file, 'utf-8')
-
-const Inline = () => [
-  style(readFile('./public/main.min.css')),
-  script(readFile('./public/app.min.js'))
-]
-
-const Reference = () => [
-  link({ rel: 'stylesheet', href: '/main.css' }),
-  script({ defer: true, src: '/app.js' })
-]
 
 const render = data => {
   return html({ lang: 'en' }, [
@@ -21,14 +13,21 @@ const render = data => {
     meta({ name: 'author', content: data.author }),
     meta({ name: 'description', content: data.description }),
     meta({ name: 'theme-color', content: '#202225' }),
-    meta({ id: 'viewport', name: 'viewport', content: 'width=device-width' }),
-    link({ rel: 'icon', type: 'image/png', href: '/favicon.png' }),
+    meta({ name: 'viewport', content: 'width=device-width' }),
+    link({ rel: 'icon', href: '/favicon.svg' }),
     link({ rel: 'manifest', href: '/manifest.webmanifest' }),
+    data.styles,
     body([
-      div({ id: 'app' }),
       noscript('Please enable JavaScript and try again.'),
-      (DEV ? Reference : Inline)(),
-      script({ defer: true, src: 'https://www.googletagmanager.com/gtm.js?id=GTM-KW3BBQZ' })
+      div({ id: 'app' }),
+      // div(),
+      // div({ foo: 'bar' }),
+      // div({ foo: 'bar' }, [div()]),
+      // div({ foo: 'bar' }, div()),
+      // div([div()]),
+      // div(div()),
+      data.scripts,
+      script({ defer: true, src: '//googletagmanager.com/gtm.js?id=GTM-KW3BBQZ' })
     ])
   ])
 }
@@ -36,7 +35,13 @@ const render = data => {
 const output = render({
   title: 'Discord Message Queue',
   author: 'Dustin Dowell',
-  description: 'Queue Discord messages. Send one per minute.'
+  description: 'Queue Discord messages. Send one per minute.',
+  styles: DEVELOPMENT
+    ? link({ rel: 'stylesheet', href: '/main.css' })
+    : style(readFileSync('./public/main.min.css', 'utf8')),
+  scripts: DEVELOPMENT
+    ? script({ defer: true, src: '/app.js' })
+    : script(readFileSync('./public/app.min.js', 'utf8'))
 })
 
 process.stdout.write('<!DOCTYPE html>' + output)
