4a76373ddf9d7ab53bfb7c3f7bd702add0489f05
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2020-12-28 01:03:01 -0600

    Update h.js

--- src/h.js
+++ src/h.js
@@ -1,33 +1,60 @@
 
-import { h as node, text as textNode } from 'superfine'
 import { renderNode, renderTextNode } from './render'
 
+const EMPTY_ARR = []
 const EMPTY_OBJ = {}
 const isArray = Array.isArray
 
-const nodeType = STATIC ? renderNode : node
-const text = STATIC ? renderTextNode : textNode
+// The functions `virtualNode` and `virtualTextNode` are based on Superfine's
+// https://github.com/jorgebucaran/superfine/blob/main/index.js
+
+// NOTES:
+// 1. These checks allow passing a node directly, outside of an array
+//   Before h('div', [ <node> ])
+//   After  h('div', <node>)
+// 2. The _node property is nessecary to have an optional props object
+//   Before h('div', {}, [ <children> ])
+//   After  h('div', [ <children> ])
+
+const virtualNode = (type, props, ch) => ({
+  type,
+  props,
+  children: isArray(ch) ? ch : ch == null ? EMPTY_ARR : [ch], // 1
+  dom: null,
+  key: props.key,
+  _node: true // 2
+})
+
+const virtualTextNode = (value, dom) => ({
+  type: value,
+  props: EMPTY_OBJ,
+  children: EMPTY_ARR,
+  dom,
+  key: null,
+  tag: 3,
+  _node: true // 2
+})
+
+// NOTE: renderNode returns an HTML string while virtualNode returns virtual
+// nodes. Likewise for renderTextNode and virtualTextNode.
+
+const node = STATIC ? renderNode : virtualNode
+const text = STATIC ? renderTextNode : virtualTextNode
+
+// Syntax variations
+// h('div')
+// h('div', { <props> })
+// h('div', { <props> }, [ <children> ])
+// h('div', [ <children> ])
 
 const h = a => (b, c) => {
   const cond = STATIC
     ? typeof b === 'string'
-    : b && b._node === true
+    : b._node === true
 
-  return b === undefined || isArray(b) || cond
-    ? privateNode(a, EMPTY_OBJ, b)
-    : privateNode(a, b, c)
+  return b == null || isArray(b) || cond
+    ? node(a, EMPTY_OBJ, b)
+    : node(a, b, c)
 }
 
-const privateNode = (a, b, c) => {
-  const target = nodeType(a, b, c)
-  target._node = true
-  return target
-}
-
-const privateText = a => {
-  const target = text(a)
-  target._node = true
-  return target
-}
-
-export { h, privateText as text }
+export { h, text }
